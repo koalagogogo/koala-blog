13e80f06437422378041cde8b9c6cc2242f2995f
fingal.luo <fingal.luo@toursforfun.cn>
2018-09-26 16:33:41 +0800

    some change

--- next/next.config.js
+++ next/next.config.js
@@ -2,9 +2,6 @@
 const webpack = require('webpack');
 const withLess = require('@zeit/next-less');
 const path = require('path');
-const glob = require('glob');
-
-const { resolve } = path;
 
 const baseURL = "http://127.0.0.1:3000";
 
